2c599ea3200d59fe6e6d4687d3e3db36408ecba6
shiba1014 <shiba.png@gmail.com>
2016-06-09 13:29:43 +0900

    Initial Commit

---
+++ PhotoMaster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoMaster
+//
+//  Created by Paul McCartney on 2016/06/09.
+//  Copyright © 2016年 shiba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
